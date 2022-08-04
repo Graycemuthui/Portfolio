98874d0e5b95bdbd194d8bece74298062cc984d7
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-04 22:51:36 +0300

    Create skills and service section

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,14 +15,55 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 }
+// REMOVE MENU BAR
+const navLink = document.querySelectorAll(".nav__link");
 
-/*==================== REMOVE MENU MOBILE ====================*/
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== ACCORDION SKILLS ====================*/
+// SKILLS
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
 
-/*==================== QUALIFICATION TABS ====================*/
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
 
-/*==================== SERVICES MODAL ====================*/
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+// SERVICES MODAL
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
